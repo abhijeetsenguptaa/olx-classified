af618e0f8695276ab5ab6e6f7d4bb206ef09696a
abhijeetsenguptaa <101327881+abhijeetsenguptaa@users.noreply.github.com>
2023-06-03 11:45:36 +0530

    basic structure created and database connected to the server

---
+++ server.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { connection } = require('./configs/connection');
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.port || 8080;
+
+app.get('/', async (req, res) => {
+    res.status(200).send({
+        status: true,
+        msg: 'Welcome to OLX Classified..'
+    })
+})
+
+
+app.listen(PORT, async () => {
+    try {
+        await connection;
+        console.log('Server is connected to the Database.');
+    } catch {
+        console.log('Server could not connect to the Database.');
+    }
+    console.log(`Server is listening to the port : ${PORT}`);
+})
